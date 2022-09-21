2d5b297d33bde308534c207fa343b74e1c358485
Pedro Martins <snitramordep@gmail.com>
2022-09-21 08:34:04 +0100

    Tweaked section transitions with AOS.

--- js/main.js
+++ js/main.js
@@ -9,7 +9,6 @@
             DOM.mapBtn = document.getElementById('mapBtn');
             DOM.scrollTop = document.getElementById('scrollTop');
             DOM.morphItems = document.getElementsByClassName('morph__item');
-            DOM.accordions = document.querySelectorAll('.accordion__title');
         },
 
         UI = {
@@ -65,12 +64,6 @@
             overlay.style.top = boundingRect.top + 2 + 'px';
         },
 
-        closeAccordions = () => {
-            for(let i = 0; i < DOM.accordions.length; i++) {
-                DOM.accordions[i].classList.contains('active') ? DOM.accordions[i].classList.remove('active') : null;
-            }
-        },
-
         showInfo = el => {
             hasOverlays ? clearOverlays() : null;
             let overlay = UI.overlay(el.dataset.info);
@@ -99,18 +92,31 @@
                     showInfo(ev.target);
                 });
             }
-
-            for(let i = 0; i < DOM.accordions.length; i++) {
-                DOM.accordions[i].addEventListener('click', ev => {
-                    closeAccordions();
-                    DOM.accordions[i].classList.toggle('active');
-                });
-            }
         },
 
         init = function() {
             cacheDOM();
             bindEvents();
+            AOS.init({
+                // Global settings:
+                disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+                startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+                initClassName: 'aos-init', // class applied after initialization
+                animatedClassName: 'aos-animate', // class applied on animation
+                useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+                disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+                debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+                throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+                              
+                // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+                offset: 120, // offset (in px) from the original trigger point
+                delay: 0, // values from 0 to 3000, with step 50ms
+                duration: 400, // values from 0 to 3000, with step 50ms
+                easing: 'ease', // default easing for AOS animations
+                once: false, // whether animation should happen only once - while scrolling down
+                mirror: false, // whether elements should animate out while scrolling past them
+                anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+              });
         };
 
 
